9e1ededde305e769c1f47c08ea21385aec18fafd
suwatchai <k_suwatchai@hotmail.com>
2019-08-12 15:58:43 +0700

    Fix stream error

--- src/Firebase_Arduino_WiFiNINA.cpp
+++ src/Firebase_Arduino_WiFiNINA.cpp
@@ -602,8 +602,6 @@ bool Firebase_Arduino_WiFiNINA::getServerResponse(FirebaseData &dataObj)
 
   dataTime = millis();
 
-  if (client.connected() && !client.available())
-    dataObj._httpCode = HTTPC_ERROR_READ_TIMEOUT;
 
   if (client.connected() && client.available())
   {
@@ -943,7 +941,7 @@ bool Firebase_Arduino_WiFiNINA::getServerResponse(FirebaseData &dataObj)
     goto EXIT_2;
   }
 
-  if (dataObj._httpCode == -1000 && dataObj._r_method == FirebaseMethod::STREAM)
+  if (dataObj._httpCode == -1000)
     flag = true;
 
   dataObj._httpConnected = false;
@@ -966,7 +964,7 @@ EXIT_2:
 
   if (dataObj._httpCode == HTTPC_ERROR_READ_TIMEOUT)
     return false;
-  return dataObj._httpCode == _HTTP_CODE_OK || (dataObj._r_method == FirebaseMethod::STREAM && dataObj._httpCode == -1000);
+  return dataObj._httpCode == _HTTP_CODE_OK || dataObj._httpCode == -1000;
 
 EXIT_3:
 
